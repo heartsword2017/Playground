eef0132a6040345fe704c2bd9f61b604baa32d57
Xinjian Yao <xyb5b@mail.missouri.edu>
2017-05-03 15:57:20 -0500

    2nd commit

---
+++ src/Folder1/test111.java
@@ -0,0 +1,7 @@
+package Folder1;
+
+/**
+ * Created by Xinjian Yao on 5/3/2017.
+ */
+public class test111 {
+}
---
+++ src/folder2/test222.java
@@ -0,0 +1,7 @@
+package folder2;
+
+/**
+ * Created by Xinjian Yao on 5/3/2017.
+ */
+public class test222 {
+}
